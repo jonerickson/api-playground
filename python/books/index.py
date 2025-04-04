fb53656453ceb0a0aa049868308f9e3924fe25a1
Jon Erickson <jon@deschutesdesigngroup.com>
2025-04-03 18:10:05 -0700

    Add auth

--- python/books/index.py
+++ python/books/index.py
@@ -1,16 +1,43 @@
 import database.sqlite as db
+from datetime import timedelta
 import os
 
 from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 from marshmallow import ValidationError
 from books.model.book import Book, BookSchema
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+    return app
 
+app = create_app()
+jwt = JWTManager(app)
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.authorization.username
+    password = request.authorization.password
+    if username != "test" or password != "test":
+        return jsonify({"error": "Bad username or password."}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify({"access_token": access_token, "type": "bearer", "expires_in": 3600})
+
+@app.route("/me", methods=["GET"])
+@jwt_required()
+def me():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+@jwt_required()
 @app.route('/books', methods=['GET'])
 def get_books():    
     return jsonify({"data": BookSchema(many=True).dump(Book.get_all())})
 
+@jwt_required()
 @app.route('/books/<int:id>', methods=['GET'])
 def get_book_by_id(id):
     book = Book.get_by_id(id)
@@ -20,6 +47,7 @@ def get_book_by_id(id):
     
     return jsonify({"data": BookSchema().dump(book)})
 
+@jwt_required()
 @app.route('/books', methods=['POST'])
 def create_book():
     try:
@@ -34,6 +62,7 @@ def create_book():
     
     return jsonify({"data": BookSchema().dump(book)}), 201
 
+@jwt_required()
 @app.route('/books/<int:id>', methods=['PUT'])
 def update_book(id):
     book = Book.get_by_id(id)
@@ -53,6 +82,7 @@ def update_book(id):
     
     return jsonify({"data": BookSchema().dump(book)})
 
+@jwt_required()
 @app.route('/books/<int:id>', methods=['DELETE'])
 def delete_book(id):
     book = Book.get_by_id(id)
@@ -70,4 +100,5 @@ print(f"""
 Environment Variables:
       
 DATABASE_PATH={os.getenv('DATABASE_PATH')}
+JWT_SECRET={os.getenv('JWT_SECRET')}
 """)
