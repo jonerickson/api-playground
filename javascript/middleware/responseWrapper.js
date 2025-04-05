1be39cf560e9e3786a6c3670594039fbfa95cead
Jon Erickson <jon@deschutesdesigngroup.com>
2025-04-05 15:28:15 -0700

    Remove comment

--- javascript/middleware/responseWrapper.js
+++ javascript/middleware/responseWrapper.js
@@ -2,7 +2,6 @@ function responseWrapper(req, res, next) {
     const oldJson = res.json;
   
     res.json = function (data) {
-      // If it's already wrapped, don't double-wrap it
       if (data && (data.status === 'okay' || data.status === 'error')) {
         return oldJson.call(this, data);
       }
